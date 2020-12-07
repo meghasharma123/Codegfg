b05c1e39c8128c005b17df9cd1a251b6aa5ee464
Megha Sharma <53375800+meghasharma123@users.noreply.github.com>
2020-12-07 12:48:06 +0530

    compiled program

--- Trapping Rain water/mt1.java
+++ Trapping Rain water/mt1.java
@@ -1,7 +1,7 @@
 //Method 1 to trap water stored after raining width of bar is constant 1.
-public class main
+public class Main
 {
-  public class maxWidth(int[] arr,int n)
+  public static int maxWidth(int[] arr,int n)
   { 
     int res =0;
     for(int i=1;i<n-1;i++)
@@ -11,12 +11,12 @@ public class main
         left = Math.max(left,arr[j]);
       int right = arr[i];
       for(int j=i+1;j<n;j++)
-        right = Math.max(right,arr[i]);
-      res += min(left,right)-arr[i];
+        right = Math.max(right,arr[j]);
+      res += Math.min(left,right)-arr[i];
     }
     return res;
   }
-  public static void main(string[] args)
+  public static void main(String[] args)
   {
     int[] arr = {0,1,2,7,5,4,1,5,0,6};
     int n=arr.length;
